a632eff73dff8a1de06aed7ee945114a07af9386
Ren510 <yt.1141977@gmail.com>
2021-04-25 13:03:58 +0000

    devedevelop

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -47,10 +47,6 @@ class Admin::CustomersController < ApplicationController
     params.require(:customer).permit(:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :is_deleted)
   end
 
-  # def hide
-  #
-  #
-
   private
   def customer_params
       params.require(:customer).permit(:last_name, :first_name, :email, :is_deleted)
